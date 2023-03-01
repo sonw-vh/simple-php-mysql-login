026a07c0d1d6f26685fe4767be10aad3816fba96
sonw-vh <me@here.com>
2023-03-01 14:19:33 +0700

    update ui

--- dashboard.php
+++ dashboard.php
@@ -1,15 +1,11 @@
 <?php
-// Start the session
 session_start();
 
-// Check if the user is logged in
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    // Redirect to the login page
     header("Location: index.php");
     exit;
 }
 
-// Get the username from the session
 $username = $_SESSION['username'];
 ?>
 
--- login.php
+++ login.php
@@ -1,8 +1,6 @@
 <?php
-// Start the session
 session_start();
 
-// Connect to the database
 $servername = "localhost";
 $username = "dibongaodu";
 $password = "@Dibongaodu123";
@@ -10,32 +8,24 @@ $dbname = "login";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Get the username and password from the form
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// Query the database for the user
 $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 $result = mysqli_query($conn, $sql);
 
-// Check if the user was found
 if (mysqli_num_rows($result) == 1) {
-    // Set session variables
     $_SESSION['username'] = $username;
     $_SESSION['loggedin'] = true;
 
-    // Redirect to the dashboard page
     header("Location: dashboard.php");
 } else {
-    // Redirect back to the login page with an error message
     header("Location: index.php?error=invalid_login");
 }
 
-// Close the database connection
 mysqli_close($conn);
 ?>
